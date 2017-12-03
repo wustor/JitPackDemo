a8d41f49283a3cf6fdfdf9e898950e99c7128238
pangchao <15527283152@163.com>
2017-12-03 14:36:50 +0800

    Initial commit

---
+++ app/src/main/java/com/wustor/jitpackdemo/MainActivity.java
@@ -0,0 +1,13 @@
+package com.wustor.jitpackdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
